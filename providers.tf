df998015410637c61527d74cd34bdd582f3594e2
Peter Griffin <peter.martin.griffin1@gmail.com>
2024-01-20 23:43:57 -0700

    Fixing s3 backend variable

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-rhhtdrmclbni"
     key    = "terraform.tfstate"
-    region = var.aws_bucket_location
+    region = "us-west-2"
   }
 
   required_providers {
